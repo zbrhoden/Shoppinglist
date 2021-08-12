37cb013eef422fc566679ad54eb2db35d60c4722
Blake Rhoden <zbrhoden@gmail.com>
2021-08-12 11:58:46 -0500

    first commit

---
+++ shopping.js
@@ -0,0 +1,42 @@
+const groceries = [
+    {
+        id: 1,
+        name: "Milk",
+        price: 5,
+    },
+    {
+        id: 2,
+        name: "Eggs",
+        price: 3,
+    },
+    {
+        id: 3,
+        name: "Pizza rolls",
+        price: 11,
+    },
+    {
+        id: 4,
+        name: "steak",
+        price: 18,
+    },
+    {
+        id: 5,
+        name: "apple",
+        price: 2,
+    },
+    {
+        id: 6,
+        name: "sandwich buns",
+        price: 7,
+    },
+    {
+        id: 7,
+        name: "ketchup",
+        price: 4,
+    }
+]
+
+for (const grocery of groceries) {
+    console.log(grocery)
+}
+
